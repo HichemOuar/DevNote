0b5c89cd950921f26df9812e0c5569c8da10d7fd
HichemOuar <96795106+HichemOuar@users.noreply.github.com>
2024-05-06 22:21:04 +0200

    Initial Commit

---
+++ src/main/java/com/example/ProjetCDA/ProjetCdaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjetCDA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetCdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetCdaApplication.class, args);
+	}
+
+}
