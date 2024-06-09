51522be2377c6f22feb2105d64edeeefae0500be
HichemOuar <96795106+HichemOuar@users.noreply.github.com>
2024-06-09 18:09:33 +0200

    mise en forme

--- src/main/java/com/example/DevNote/controller/UsersController.java
+++ src/main/java/com/example/DevNote/controller/UsersController.java
@@ -9,14 +9,14 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-@RestController // Cette annotation indique que la classe est un contrôleur REST.REST est une manière standard de structurer les communications entre un client (comme un navigateur web ou
-// une application mobile) et un serveur. Un service web RESTful permet au client de récupérer ou modifier des informations sur le serveur à travers des requêtes HTTP standards
-// (comme GET pour récupérer des informations ou POST pour envoyer des informations).
+@RestController // Cette annotation indique que la classe est un contrôleur REST.REST est une manière standard de structurer les communications entre un client (comme un
+// navigateur web ou une application mobile) et un serveur. Un service web RESTful permet au client de récupérer ou modifier des informations sur le serveur à travers des
+// requêtes HTTP standards (comme GET pour récupérer des informations ou POST pour envoyer des informations).
 // Le @RestController simplifie le code nécessaire pour envoyer les données directement à qui les demande (par exemple, le navigateur ou une application mobile.
 //Contrairement à certains autres contrôleurs qui renvoient des pages web complètes (avec HTML, CSS, etc.), un @RestController renvoie généralement juste des données.
-// Ces données peuvent être sous forme de texte simple, JSON, ou XML. En renvoyant des données au lieu de HTML, les @RestControllers sont très utiles quand on veut que le backend
-// soit séparé du frontend. Cela permet aux développeurs de frontend de construire l'interface utilisateur comme ils le souhaitent, en utilisant les données reçues via les @RestController
-// (et aussi certains problèmes de sécurité)
+// Ces données peuvent être sous forme de texte simple, JSON, ou XML. En renvoyant des données au lieu de HTML, les @RestControllers sont très utiles quand on veut que le
+// backend soit séparé du frontend. Cela permet aux développeurs de frontend de construire l'interface utilisateur comme ils le souhaitent, en utilisant les données reçues
+// via les @RestController (et aussi certains problèmes de sécurité)
 
 @RequestMapping("/api/users") // Définit l'URL de base pour toutes les requêtes gérées par ce contrôleur. Ainsi, toutes les méthodes de ce contrôleur commenceront par /api/users.
 public class UsersController {
